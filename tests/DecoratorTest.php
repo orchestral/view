539f655f19c5369946479aa37bae015a79bf6288
crynobone <crynobone@gmail.com>
2013-05-08 19:37:12 +0800

    Move Orchestra\Support\Decorator to Orchestra\View component orchestral/support#15
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ tests/DecoratorTest.php
@@ -0,0 +1,33 @@
+<?php namespace Orchestra\View\Tests;
+
+use Orchestra\View\Decorator;
+
+class DecoratorTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * Test add and using macros.
+	 *
+	 * @test
+	 */
+	public function testAddAndUsingMacros()
+	{
+		$stub = new Decorator;
+		
+		$stub->macro('foo', function ()
+		{
+			return 'foo';
+		});
+
+		$this->assertEquals('foo', $stub->foo());
+	}
+
+	/**
+	 * Test calling undefined macros throws an exception.
+	 *
+	 * @expectedException \BadMethodCallException
+	 */
+	public function testCallingUndefinedMacrosThrowsException()
+	{
+		with(new Decorator)->foobar();
+	}
+}
