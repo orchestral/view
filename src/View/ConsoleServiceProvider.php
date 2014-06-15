4fb90379a5648daff12f5b5464eaf702d8fd67fd
crynobone <crynobone@gmail.com>
2014-06-15 10:52:52 +0800

    Add `php artisan theme` to view current activated theme.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/View/ConsoleServiceProvider.php
@@ -0,0 +1,23 @@
+<?php namespace Orchestra\View;
+
+use Illuminate\Support\ServiceProvider;
+use Orchestra\View\Console\StatusCommand;
+
+class ConsoleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bindShared('orchestra.view.command.status', function ($app) {
+            return new StatusCommand($app['orchestra.memory']);
+        });
+
+        $this->commands(array(
+            'orchestra.view.command.status',
+        ));
+    }
+}
