0bec09e5996707769ccfc35bd8f3f726dc94268c
crynobone <crynobone@gmail.com>
2014-06-16 01:15:26 +0800

    Port over command to 2.1
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/View/CommandServiceProvider.php
@@ -0,0 +1,33 @@
+<?php namespace Orchestra\View;
+
+use Illuminate\Support\ServiceProvider;
+use Orchestra\View\Console\ActivateCommand;
+use Orchestra\View\Console\DetectCommand;
+
+class CommandServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bindShared('orchestra.view.command.detect', function ($app) {
+            $finder = $app['orchestra.theme.finder'];
+
+            return new DetectCommand($finder);
+        });
+
+        $this->app->bindShared('orchestra.view.command.activate', function ($app) {
+            $finder = $app['orchestra.theme.finder'];
+
+            return new ActivateCommand($finder);
+        });
+
+        $this->commands(array(
+            'orchestra.view.command.detect',
+            'orchestra.view.command.activate',
+        ));
+    }
+}
---
+++ src/View/Console/DetectCommand.php
@@ -0,0 +1,67 @@
+<?php namespace Orchestra\View\Console;
+
+use Orchestra\View\Theme\Finder;
+
+class DetectCommand extends BaseCommand
+{
+    /**
+     * Theme finder instance.
+     *
+     * @var \Orchestra\View\Theme\Finder
+     */
+    protected $finder;
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'theme:detect';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Detect available themes in the application.';
+
+    /**
+     * Construct a new status command.
+     *
+     * @param  \Orchestra\View\Theme\Finder $finder
+     */
+    public function __construct(Finder $finder)
+    {
+        $this->finder = $finder;
+
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+        $memory   = $this->laravel['orchestra.memory'];
+
+        $themes   = $this->finder->detect();
+        $frontend = $memory->get('site.theme.frontend');
+        $backend  = $memory->get('site.theme.backend');
+
+        $header  = array('ID', 'Theme Name', 'Frontend', 'Backend');
+        $content = array();
+
+        foreach ($themes as $id => $theme) {
+            $content[] = array(
+                $id,
+                $theme->name,
+                ($id == $frontend ? "   ✓" : ''),
+                ($id == $backend ? "   ✓" : ''),
+            );
+        }
+
+        $this->table($header, $content);
+    }
+}
