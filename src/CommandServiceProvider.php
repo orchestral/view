3e7cb81db473d9e9ac48e4ed569ed55c9a1eb75e
crynobone <crynobone@gmail.com>
2015-05-17 12:30:56 +0800

    Use available base command service provider.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/CommandServiceProvider.php
+++ src/CommandServiceProvider.php
@@ -1,60 +1,60 @@
 <?php namespace Orchestra\View;
 
-use Illuminate\Support\ServiceProvider;
 use Orchestra\View\Console\DetectCommand;
 use Orchestra\View\Console\ActivateCommand;
 use Orchestra\View\Console\OptimizeCommand;
+use Orchestra\Support\Providers\CommandServiceProvider as ServiceProvider;
 
 class CommandServiceProvider extends ServiceProvider
 {
     /**
-     * Indicates if loading of the provider is deferred.
+     * The commands to be registered.
      *
-     * @var bool
+     * @var array
      */
-    protected $defer = true;
+    protected $commands = [
+        'Activate' => 'orchestra.view.command.activate',
+        'Detect'   => 'orchestra.view.command.detect',
+        'Optimize' => 'orchestra.view.command.optimize'
+    ];
 
     /**
      * Register the service provider.
      *
      * @return void
      */
-    public function register()
+    public function registerActivateCommand()
     {
         $this->app->singleton('orchestra.view.command.activate', function ($app) {
             $finder = $app['orchestra.theme.finder'];
 
             return new ActivateCommand($finder);
         });
+    }
 
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function registerDetectCommand()
+    {
         $this->app->singleton('orchestra.view.command.detect', function ($app) {
             $finder = $app['orchestra.theme.finder'];
 
             return new DetectCommand($finder);
         });
-
-        $this->app->singleton('orchestra.view.command.optimize', function () {
-            return new OptimizeCommand();
-        });
-
-        $this->commands([
-            'orchestra.view.command.activate',
-            'orchestra.view.command.detect',
-            'orchestra.view.command.optimize',
-        ]);
     }
 
     /**
-     * Get the services provided by the provider.
+     * Register the service provider.
      *
-     * @return array
+     * @return void
      */
-    public function provides()
+    public function registerOptimizeCommand()
     {
-        return [
-            'orchestra.view.command.activate',
-            'orchestra.view.command.detect',
-            'orchestra.view.command.optimize',
-        ];
+        $this->app->singleton('orchestra.view.command.optimize', function () {
+            return new OptimizeCommand();
+        });
     }
 }
